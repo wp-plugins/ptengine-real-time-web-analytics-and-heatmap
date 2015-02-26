d9e2efeba86fddb296f0e88887bff929f481afb5
ptengine <ptengine@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-26 07:00:33 +0000

    modify readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/ptengine-real-time-web-analytics-and-heatmap/trunk@1100035 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ptengine-for-wordpress.php
+++ ptengine-for-wordpress.php
@@ -3,7 +3,7 @@
  * Plugin Name: Ptengine - Real time web analytics and Heatmap
  * Version: 1.0.0
  * Plugin URI: http://www.ptengine.com/
- * Description: To get started: go to <a href="/wp-admin/admin.php?page=ptengine_setting">option page</a> and (1) sign up (2) create a profile (3) start to see your real time traffic! Go to Ptengine for the full version if you want more conversions.
+ * Description: To get started: activate this plugin, go to <a href="admin.php?page=ptengine_setting">option page</a> and (1) sign up (2) create a profile (3) start to see your real time traffic! Go to Ptengine for the full version if you want more conversions.
  * Author: Ptengine
  * Author URI: http://www.ptengine.com/
  */
